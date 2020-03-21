c30adbfe550898a0c8b226f4c81528cd2522b66b
PAcee1 <8709867@qq.com>
2020-03-21 16:18:52 +0800

    add error code and test sonar

---
+++ src/main/java/com/enbuys/jenkins/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.enbuys.jenkins;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // 错误代码
+        int i = 100/0;
+
+        // 冗余代码
+        int a = 1;
+        a = 2;
+    }
+}
